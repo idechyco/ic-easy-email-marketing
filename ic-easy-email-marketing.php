c1d357585704e5180a6e5fad747e4b28ddc4aa91
idechyco <167994543+idechyco@users.noreply.github.com>
2024-06-11 11:36:48 +0330

    Create ic-easy-email-marketing.php

---
+++ ic-easy-email-marketing.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name:  Ic Easy Email Marketing
+Plugin URI:   https://idechy.ir/
+Description:  ایمیل اختصاصی ویزا لاین
+Version:      1.0
+Author:       idechy
+Author URI:   https://idechy.ir/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wpb-tutorial
+Domain Path:  /languages
+*/
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit();
+  }
